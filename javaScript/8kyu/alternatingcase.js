b313bc541d3c409579d704b302d958dae2cc84c2
Ali Raza Ashraf <adaydreamer10@gmail.com>
2023-12-02 10:22:14 +0100

    Alternating case

---
+++ javaScript/8kyu/alternatingcase.js
@@ -0,0 +1,36 @@
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details)
+// such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+
+
+// My Solution
+
+String.prototype.toAlternatingCase = function () {
+    return this.split('').map(char => {
+      return char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+    }).join('');
+  }
+
+// Other Solution
+
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+}
+
+// Other Solution using foor loop
+
+String.prototype.toAlternatingCase = function () {
+    new_str = "";
+    for(var i = 0; i < this.length; i++) {
+      if(this[i] === this[i].toUpperCase()) {
+        new_str += this[i].toLowerCase();
+      }
+      else {
+        new_str += this[i].toUpperCase();
+      }
+    }
+    return new_str;
+  }
