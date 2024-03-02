d5a1d24a2f4769249614b878cfc9453188fa74a9
Ali Raza Ashraf <adaydreamer10@gmail.com>
2024-03-02 23:56:22 +0100

    Pillars kata using If else

--- javaScript/8kyu/pillars.js
+++ javaScript/8kyu/pillars.js
@@ -11,3 +11,13 @@ function pillars(num_pill, dist, width) {
     // your code here
     return num_pill>1?(num_pill-1)*dist*100+(num_pill-2)*width:0;
   }
+
+// Other Solution
+
+function pillars(num_pill, dist, width) {
+    if (num_pill < 2) return 0;
+    const widthAllPillars = (num_pill - 2) * width;
+    const distAll = (num_pill - 1) * (dist * 100);
+    
+    return distAll + widthAllPillars;
+  }
